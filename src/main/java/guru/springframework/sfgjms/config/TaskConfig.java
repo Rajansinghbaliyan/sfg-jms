1b5636eb45c2a9bed0e571abff8e3a4a8baccb0f
rbaliyancosm <87328960+rbaliyancosm@users.noreply.github.com>
2022-01-13 17:58:15 +0530

    feat: adding configuration for the schedule tasks

--- src/main/java/guru/springframework/sfgjms/config/TaskConfig.java
+++ src/main/java/guru/springframework/sfgjms/config/TaskConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @EnableAsync
+@EnableScheduling
 @Configuration
 public class TaskConfig {
 
