f77c376bc8e8a318069d47d8c37723e88f3755fd
rbaliyancosm <87328960+rbaliyancosm@users.noreply.github.com>
2022-01-14 06:40:07 +0530

    feat: adding configuration for the Jms config

---
+++ src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class HelloWorldMessage {
+    private UUID uuid;
+    private String message;
+}
